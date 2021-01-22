72d971da075eb88806c5ec7bb81d562d2cc9acde
Kroshka Ru <rururubell@gmail.com>
2021-01-22 16:50:00 +0500

    Delete my_run.pl

--- my_run.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-
-use strict;
-use warnings;
-use utf8;
-
-
-system( "/home/daniel/Documents/Other/renpy/renpy-7.3.5-sdk/renpy.sh /home/daniel/Documents/Sources/renpy/projects/radiopulsarvn/game" );
